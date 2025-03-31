8899f1441ec667d08dfa5bbd43fecec02a5fc795
Tuan Minh Nguyen <tuanmminh.101112@gmail.com>
2025-03-31 12:25:11 +0700

    update /src

--- flatform/airflow/dags/dag.py
+++ flatform/airflow/dags/dag.py
@@ -5,12 +5,19 @@ from datetime import timedelta, datetime
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from pathlib import Path
+import os
 
 def create_temp_dir():
 	"""
 	"""
-	path_save = Path("./cls_temp_dir")
+	path_save = Path("./cls_tmp_dir")
+	
+	# if path_save.exists():
+	# 	cmd_0  = f"rm -rf {path_save}"
+	# 	os.system(cmd_0)
+
 	path_save.mkdir(parents=True, exist_ok=True)
+
 	if not path_save.exists():
 		raise FileExistsError(f"Failed to create directory: {path_save}")
 	print(path_save)
@@ -28,28 +35,33 @@ with DAG(
     default_args= default_args,
     description= "A simple classify pipeline",
 ) as dag:
-
-    create_temp_dir_task = PythonOperator(task_id="create_temp_dir",
-									 python_callable=create_temp_dir,
-									 dag=dag)
 	
-    processing_task = BashOperator(task_id="process_data",
-								   bash_command=("cd / && cd opt/airflow && python3 ./src/processing.py" \
+	whoami_task = BashOperator(task_id="check_user",
+    							bash_command="whoami",
+    							dag=dag)
+	
+	create_temp_dir_task = PythonOperator(task_id="create_temp_dir",
+								  python_callable=create_temp_dir,
+								  dag=dag)
+	
+	processing_task = BashOperator(task_id="process_data",
+								   bash_command=("cd / && cd opt/airflow && python3 src/processing.py" \
 						 			" --config-file='./config/parameters.yaml'"),
 									dag=dag)
-									
 	
-    training_task = BashOperator(task_id="train_model",
-								 bash_command=("cd / && cd opt/airflow && python3 ./src/train.py" \
+	training_task = BashOperator(task_id="train_model",
+								 bash_command=("cd / && cd opt/airflow && python3 src/train.py" \
 					                " --config-file='./config/parameters.yaml'"),
 									dag=dag)
 	
-    registered_task = BashOperator(task_id="registered_model",
-								  bash_command=("cd / && cd opt/airflow && python3 ./src/register.py" \
+	registered_task = BashOperator(task_id="registered_model",
+								  bash_command=("cd / && cd opt/airflow && python3 src/register.py" \
 						            " --config-file='./config/parameters.yaml'"),
 									dag=dag)
 	
 
-create_temp_dir_task >> processing_task >> training_task >> registered_task
+
+whoami_task >> create_temp_dir_task >> processing_task >> training_task >> registered_task
+
 
 							 
--- flatform/airflow/src/processing.py
+++ flatform/airflow/src/processing.py
@@ -10,13 +10,23 @@ def main(args):
 	TMP_DIR = Path(args.tmp_dir)
 
 	DATA_DIR = SRC_DIR / args.data
+	
+	csv_file = TMP_DIR / "train.csv"
 
-	cmd_0 = f"unzip {DATA_DIR} -d {TMP_DIR}"
-	os.system(cmd_0)
+	#os.system(f"sudo chown -R $(whoami):$(whoami) {DATA_DIR}") 
+	cmd_1 = f"chown -R $(whoami):$(whoami) ./opt/airflow/DATA/{args.data}" #"chmod u+w ~/cls_temp_dir"
+	#cmd_2 = "ls -l"
+	cmd_3 = f"unzip {DATA_DIR} -d {TMP_DIR}"
+	#os.system(cmd_0)
 
-	# train_loader, val_loader, data_test = build_dataset(args)
+	os.system(cmd_1)
 
-	# return train_loader, val_loader, data_test
+	if csv_file.exists():
+		print(f"File {csv_file} is existed!!!")
+	
+	else:		
+		os.system(cmd_3)
+	
 
 
 if __name__ == "__main__":
